2954caac708db9b15580597f13885b5e51f07b29
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-14 01:28:06 +0300

    Update ex_5(month).js

--- ex_5(month).js
+++ ex_5(month).js
@@ -1,1 +1,16 @@
 // Найти по заданному номеру месяца, количество дней в нем
+
+let monthNumber = 11;
+//let daysInMonth;
+
+let d31 = [1, 3, 5, 7, 8, 10, 12];
+let d30 = [4, 6, 9, 11];
+if (d31.includes(monthNumber)) {
+  console.log("31 days");
+} else if (d30.includes(monthNumber)) {
+  console.log("30 days");
+} else {
+  console.log("28 days (or 29 if it's leap year)");
+}
+//console.log("Number of days in a month: ", daysInMonth);
+//console.log(d31.includes(monthNumber));
