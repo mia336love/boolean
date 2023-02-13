f7a7eef15843cb4ab5cf02c10256936533221a0d
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-14 02:36:03 +0300

    Update ex_4(operation).js

---
+++ ex_4(operation).js
@@ -0,0 +1,25 @@
+// написать программу, по заданным переменным: число1, число2, операция -> выполнить базовую математическую операцию
+
+let num1 = 6;
+let num2 = 2;
+let operation = num1 / num2;
+
+switch (operation) {
+  case 1:
+    alert("Answer: 1");
+    break;
+  case 2:
+    alert("Answer: 2");
+    break;
+  case 3:
+    alert("Answer: 3");
+    break;
+  case 4:
+    alert("Answer: 4");
+    break;
+  case 5:
+    alert("Answer: 5");
+    break;
+  default:
+    alert("There is no such value");
+}
